29f5fe3b9686e9840bc4d74f91de5204c4e71a15
Rassep <77835262+Rassep@users.noreply.github.com>
2021-01-25 13:48:32 +0200

    Add files via upload

---
+++ Harjoitus 1/Ajoneuvo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Harjoitus1
+{
+    class Ajoneuvo
+    {
+        public string nimi { get; set; }
+        
+        public int nopeus { get; set; }
+        public int renkaat { get; set; }
+
+        public void TulostaData()
+        {
+            Console.WriteLine(ToString());
+        }
+
+        
+        override public string ToString()
+        {
+            return string.Format("Nimi: {0}, Nopeus: {1}, Renkaat: {2}", nimi, nopeus.ToString(), renkaat.ToString());
+        }
+    }
+}
---
+++ Harjoitus 1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Harjoitus1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ajoneuvo ajoneuvo = new Ajoneuvo();
+            ajoneuvo.nimi = "Ford";
+            ajoneuvo.nopeus = 150;
+            ajoneuvo.renkaat = 4;
+
+            ajoneuvo.TulostaData();
+            string strAjoneuvo = ajoneuvo.ToString();
+            Console.WriteLine(string.Format("strAjoneuvo: {0}", strAjoneuvo));
+        }
+    }
+}
